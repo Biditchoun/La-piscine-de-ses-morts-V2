9ab5919f894692a519301a258b61d7ccec366618
Sam Wijnbergen <sawijnbe@shi-r5-p3.s19.be>
2025-07-12 15:46:50 +0200

    Rush finished, probably

---
+++ Rush00/rush00.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sawijnbe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/12 14:40:22 by sawijnbe          #+#    #+#             */
+/*   Updated: 2025/07/12 14:56:06 by sawijnbe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_line(int l, char c1, char c2, char c3)
+{
+	int	i;
+
+	write(1, &c1, 1);
+	i = 1;
+	while (l - ++i > 0)
+		write(1, &c2, 1);
+	if (l > 1)
+		write(1, &c3, 1);
+	write(1, "\n", 1);
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+
+	if (x < 1 || y < 1)
+		return ;
+	print_line(x, 'o', '-', 'o');
+	i = 1;
+	while (y - ++i > 0)
+		print_line(x, '|', ' ', '|');
+	if (y > 1)
+		print_line(x, 'o', '-', 'o');
+}
